f468448e2e68302a273f650c31ba8432b58b320c
Florian Portari <floryanportar@gmail.com>
2022-07-15 18:18:04 +0200

    resolved. Tests passed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function (degreesFah) {
+  let calc = ((degreesFah - 32) * 5) / 9;
+  let resultRounded = Math.round(calc * 10) / 10;
+  return resultRounded;
 };
 
-const ctof = function() {
-
+const ctof = function (degreesCel) {
+  let calc = degreesCel * (9 / 5) + 32;
+  let resultRounded = Math.round(calc * 10) / 10;
+  return resultRounded;
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };

c431cf825594c403914541363e8752763c03bcc1
Florian Portari <floryanportar@gmail.com>
2022-07-15 16:55:17 +0200

    resolved. Tests passed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let splitStr = str.split("");
+  let reverseStr = splitStr.reverse();
+  let joinStr = reverseStr.join("");
+  return joinStr;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (arr, ...value) {
+  return arr.filter((num) => {
+    return !value.includes(num);
+  });
 };
 
 // Do not edit below this line

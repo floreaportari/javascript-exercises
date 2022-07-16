6bfe6e3383db298b054ed162e9ce696702265b0f
Florian Portari <floryanportar@gmail.com>
2022-07-16 15:27:20 +0200

    resolved. Passed tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  if (n < 0) {
+    return "OOPS";
+  }
+  if (n === 0) {
+    return 0;
+  }
 
+  const init = [0, 1];
+  for (let i = 1; i < n; i++) {
+    init.push(init[i - 1] + init[i]);
+  }
+
+  return init[init.length - 1];
 };
 
 // Do not edit below this line
--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function (arr) {
+  const newArr = [];
+  arr.forEach((el) => {
+    newArr.push(el.title);
+  });
+  return newArr;
 
+  // OR
+  //   return arr.map((book) => book.title);
 };
 
 // Do not edit below this line
